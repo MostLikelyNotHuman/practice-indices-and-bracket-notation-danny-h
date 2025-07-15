9ef956e921ab32d9ff03abaec157cd307bd74647
NotHuman <dannypercival14@gmail.com>
2025-07-15 03:11:50 -0500

    committed the sin of forgetting a semicolon. oops

--- using-arrays-1.js
+++ using-arrays-1.js
@@ -4,7 +4,7 @@ let booksArray = [
     ["A Tale of Two Cities", "The Little Prince", "The Alchemist"],
     ["Harry Potter and the Philosopher's Stone", "And Then There Were None", "Dream of the Red Chamber"],
     ["The Hobbit", "Alice's Adventures in Wonderland", "She: A History of Adventure"]
-]
+];
 
 //Access and log all elements using bracket notation with numbers
 console.log(`The books on shelf 1 are: ${booksArray[0][0]}, ${booksArray[0][1]}, ${booksArray[0][2]}`);
@@ -14,7 +14,7 @@ console.log(`The books on shelf 3 are: ${booksArray[2][0]}, ${booksArray[2][1]},
 //Access and log all the elements in the array using bracket notation with variables as indices
 
 for (let row = 0; row < booksArray.length; row++) {
-    console.log(`The books on shelf ${row + 1} are: `)
+    console.log(`The books on shelf ${row + 1} are: `);
     for (let item = 0; item < booksArray[row].length; item++) {
         console.log(booksArray[row][item]);
     }
